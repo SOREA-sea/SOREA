774265129e9ea272b3c35a29e8452832f7dfa854
Benladamm <yyy618442@gmail.com>
2026-05-15 11:18:27 +0200

    Ajout de Suspense pour le chargement des pages Coaching et Login

--- src/app/Coaching/page.tsx
+++ src/app/Coaching/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import CoachingPage from '@/components/Coaching';
 
 export default function CoachingRoute() {
-  return <CoachingPage />;
+  return (
+    <Suspense fallback={<div>Chargement...</div>}>
+      <CoachingPage />
+    </Suspense>
+  );
 }
