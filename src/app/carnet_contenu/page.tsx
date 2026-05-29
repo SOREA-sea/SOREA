1b59ceabef0bf94a4547960fc82ea3d5ae5a9547
Enzo Colombet Descorps <enzo2colombet@gmail.com>
2026-05-29 12:51:29 +0200

    Carnet contenu

---
+++ src/app/carnet_contenu/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export default function AboutPage() {
+    return (
+        <main className="p-8 bg-[#fdfafb] min-h-screen">
+            <h1 className="text-2xl font-bold mb-8">Mon carnet — privé</h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+
+
+            </div>
+        </main>
+    );
+}
