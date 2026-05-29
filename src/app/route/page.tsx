c9e5207923f0d90582918711c50ac45834692466
caissani2-dev <caissani2@outlook.com>
2026-05-29 11:33:40 +0200

    fix challenge

--- src/app/route/page.tsx
+++ src/app/route/page.tsx
@@ -8,19 +8,26 @@ export default function MiroirDesAffirmations() {
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-purple-50 to-[#f9f5fa] font-sans text-gray-800 relative items-center">
       
       <div className="w-[1440px] pr-[96px] pl-[96px] mx-auto pb-[24px] gap-[50px]">
-      <Navbar />
-    </div>
+        <Navbar />
+      </div>
 
-    
       <main className="flex flex-col flex-grow items-center mx-auto w-[1440px] pt-[150px] pr-[96px] pb-[24px] pl-[96px]">
+
+        
+        <div className="w-full mb-6">
+          <Link href="/challenge">
+            <button className="flex items-center gap-2 bg-white text-[#8B47FF] font-bold px-6 py-3 rounded-2xl shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl cursor-pointer border-2 border-[#8B47FF]">
+              ← Retour
+            </button>
+          </Link>
+        </div>
+
         <h1 className="text-4xl font-bold text-black mb-6 underline">
           Relève tes défis bien-être avec SOREA
         </h1>
       </main>
 
-    <Wheeltestcopy />
-
-
+      <Wheeltestcopy />
 
       <div className="w-[1440px] pr-[96px] pl-[96px] mx-auto pb-[24px]">
         <Footer />
